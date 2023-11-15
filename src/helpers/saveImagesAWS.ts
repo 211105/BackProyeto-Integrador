9aa1aef6064e4cf6f46d8fa99a9628b95915a378
nombre <211102@ids.upchiapas.edu.mx>
2023-11-15 04:11:03 -0600

    Ya se sube imagenes a S3

--- src/helpers/saveImagesAWS.ts
+++ src/helpers/saveImagesAWS.ts
@@ -1,5 +1,6 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand,DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { Credentials } from '@aws-sdk/types';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";  // Importa la función para firmar URL
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -22,10 +23,34 @@ export async function uploadToS3(fileBuffer: Buffer, key: string, contentType: s
     ContentType: contentType,
   };
 
-  // Subir el archivo a S3
-  await s3Client.send(new PutObjectCommand(uploadParams));
+  try {
+    // Subir el archivo a S3
+    await s3Client.send(new PutObjectCommand(uploadParams));
 
-  // Construir y devolver la URL de la imagen en S3
-  const s3ImageUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.BUCKET_REGION}.amazonaws.com/${key}`;
-  return s3ImageUrl;
+    // Generar una URL firmada que expire en 5 minutos (ajusta según sea necesario)
+    const command = new GetObjectCommand({
+      Bucket: process.env.BUCKET_NAME || '',
+      Key: key,
+    });
+    const signedUrl = await getSignedUrl(s3Client, command);
+
+    return signedUrl;
+  } catch (error) {
+    console.error("Error uploading to S3:", error);
+    throw error; // Re-lanzar el error para que se maneje en el controlador
+  }
 }
+
+export async function deleteFromS3(key: string): Promise<void> {
+    try {
+      const deleteParams = {
+        Bucket: process.env.BUCKET_NAME || '',
+        Key: key,
+      };
+  
+      await s3Client.send(new DeleteObjectCommand(deleteParams));
+    } catch (error) {
+      console.error("Error deleting from S3:", error);
+      throw error;
+    }
+  }
