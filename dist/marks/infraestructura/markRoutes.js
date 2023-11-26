e56785ad8fdc56ba8d67adcc138e9757c23e2456
211099 <211099@ids.upchiapas.edu.mx>
2023-11-26 05:58:10 -0600

    cambio de respuesta de rutina, el anterior estaba mal

--- dist/marks/infraestructura/markRoutes.js
+++ dist/marks/infraestructura/markRoutes.js
@@ -7,7 +7,9 @@ exports.markRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const dependencies_1 = require("./dependencies");
 exports.markRouter = express_1.default.Router();
-exports.markRouter.get('/rutine', (req, res) => { return res.status(200); });
+exports.markRouter.get('/rutine', (req, res) => {
+    res.status(200).send('Rutina ejecutada con éxito');
+});
 exports.markRouter.post('/', dependencies_1.createMarkController.run.bind(dependencies_1.createMarkController));
 exports.markRouter.get('/', dependencies_1.listMarkController.run.bind(dependencies_1.listMarkController));
 exports.markRouter.post('/assist', dependencies_1.userAssistController.run.bind(dependencies_1.userAssistController));
--- src/marks/infraestructura/markRoutes.ts
+++ src/marks/infraestructura/markRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { createMarkController, listMarkController, userAssistController } from "./dependencies";
 import { validateToken } from "../../helpers/veryfyToken";
-
+import { Request, Response } from "express";
 
 export const markRouter = express.Router();
-
-markRouter.get('/rutine', (req,res) => {return res.status(200)})
+markRouter.get('/rutine', (req: Request, res: Response) => {
+    res.status(200).send('Rutina ejecutada con éxito');
+})
 markRouter.post('/',createMarkController.run.bind(createMarkController))
 markRouter.get('/',listMarkController.run.bind(listMarkController))
 markRouter.post('/assist',userAssistController.run.bind(userAssistController))
