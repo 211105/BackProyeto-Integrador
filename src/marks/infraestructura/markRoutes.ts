b9289723987fe9f3012435123847e69768c7a1c9
211099 <211099@ids.upchiapas.edu.mx>
2023-11-26 05:49:45 -0600

    cambio de respuesta al endpoint que verifica la instancia

--- src/marks/infraestructura/markRoutes.ts
+++ src/marks/infraestructura/markRoutes.ts
@@ -5,7 +5,7 @@ import { validateToken } from "../../helpers/veryfyToken";
 
 export const markRouter = express.Router();
 
-markRouter.get('/rutine', (req,res) => {return res.status(201)})
+markRouter.get('/rutine', (req,res) => {return res.status(200)})
 markRouter.post('/',createMarkController.run.bind(createMarkController))
 markRouter.get('/',listMarkController.run.bind(listMarkController))
 markRouter.post('/assist',userAssistController.run.bind(userAssistController))
