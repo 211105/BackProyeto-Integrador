1608d11c0e9948b4e98b53e195f0210390a7688a
nombre <211102@ids.upchiapas.edu.mx>
2023-11-26 22:34:51 -0600

    Se actualizo note

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import * as serviceAccount from "./helpers/integrador-image-firebase-adminsdk-17
 import * as admin from "firebase-admin";
 import fileUpload from 'express-fileupload';
 import { noteRoutes } from "./note/infraestructure/notesRoutes";
-import { fileRoutes } from "./file/infraestructure/fileRoutes";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
@@ -28,8 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/notes', noteRoutes);
-app.use('/api/v1/files', fileRoutes);
-
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
