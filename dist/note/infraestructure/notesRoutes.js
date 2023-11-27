9cbdf62e78704ced16cfe6517c2c563c5c30ca98
211099 <211099@ids.upchiapas.edu.mx>
2023-11-26 20:28:37 -0600

    se agrego endpoint rutine

--- dist/note/infraestructure/notesRoutes.js
+++ dist/note/infraestructure/notesRoutes.js
@@ -7,6 +7,9 @@ exports.noteRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const dependencies_1 = require("./dependencies");
 exports.noteRoutes = express_1.default.Router();
+exports.noteRoutes.get('/rutine2', (req, res) => {
+    res.status(200).send('Rutina ejecutada con éxito');
+});
 exports.noteRoutes.delete('/:uuid', dependencies_1.deleteFileController.delete.bind(dependencies_1.deleteFileController));
 exports.noteRoutes.post('/create/', dependencies_1.createNoteController.post.bind(dependencies_1.createNoteController));
 exports.noteRoutes.put('/update/', dependencies_1.updateNoteController.update.bind(dependencies_1.updateNoteController));
