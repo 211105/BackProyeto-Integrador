a3b8dc03cbf6cf1874ec79a1967c44b619bd5ffb
nombre <211102@ids.upchiapas.edu.mx>
2023-11-28 04:02:36 -0600

    corriginedo ownwe

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { Signale } from 'signale';
-import { userRoutes } from "./user/infraestructure/userRouter";
 import * as serviceAccount from "./helpers/integrador-image-firebase-adminsdk-17aek-114f65daa8.json";
 import * as admin from "firebase-admin";
 import fileUpload from 'express-fileupload';
-import { driverRoutes } from "./DriversManagement/driver/infraestructure/driverRoutes";
-import { vehicleRoutes } from "./DriversManagement/vehicle/infraestructure/vehicleRoutes";
-import { ownerRoutes } from "./DriversManagement/owner/infraestructure/ownerRoutes";
+
 
 
 
@@ -26,10 +23,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/drivers', driverRoutes);
-app.use('/api/v1/vehicles', vehicleRoutes);
-app.use('/api/v1/owners', ownerRoutes);
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
