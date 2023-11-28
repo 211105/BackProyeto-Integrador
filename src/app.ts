2982a3265d25a110311ec91e8a2d0fa6ac1d1a50
nombre <211102@ids.upchiapas.edu.mx>
2023-11-28 03:59:49 -0600

    Entidad vehicle terminado

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { Signale } from 'signale';
-import { userRoutes } from "./user/infraestructure/userRouter";
 import * as serviceAccount from "./helpers/integrador-image-firebase-adminsdk-17aek-114f65daa8.json";
 import * as admin from "firebase-admin";
 import fileUpload from 'express-fileupload';
-import { driverRoutes } from "./driver/infraestructure/driverRoutes";
 import { vehicleRoutes } from "./vehicle/infraestructure/vehicleRoutes";
-import { ownerRoutes } from "./owner/infraestructure/ownerRoutes";
-import { driver_vehicleRoutes } from "./driver-vehicle/infraestructure/driver-vehicleRoutes";
-import { commentRouter } from "./comment/infraestructure/commentRouter";
 
 
 
@@ -28,12 +23,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/drivers', driverRoutes);
+
 app.use('/api/v1/vehicles', vehicleRoutes);
-app.use('/api/v1/owners', ownerRoutes);
-app.use('/api/v1/driver_vehicles', driver_vehicleRoutes);
-app.use('/api/v1/comments', commentRouter);
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- src/driver-vehicle/domain/driver_vehicleRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Driver_Vehicle,RegisterDriverVehicle } from "./driver_vehicle";
-
-export interface Driver_VehicleRepository {
-    registerDriver_Vehicle(uuid: string, driver_uuid: string, vehicle_uuid: string, status: boolean): Promise<RegisterDriverVehicle | null | string | Error>;
-
-    deleteRegisterDV(uuid:string):Promise<RegisterDriverVehicle | null | string | Error>;
-}
