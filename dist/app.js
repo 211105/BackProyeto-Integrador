dbf6f461fdf4e3737a59c2803753148a6596cb5d
211099 <211099@ids.upchiapas.edu.mx>
2023-12-02 01:08:42 -0600

    configuracion base de las rutas principales

--- dist/app.js
+++ dist/app.js
@@ -12,11 +12,11 @@ const signale = new signale_1.Signale();
 dotenv_1.default.config();
 const PORT = process.env.PORT || 3000;
 app.use('/api/v1/users', (0, express_http_proxy_1.default)('https://user.cristilex.com'));
-app.use('/api/v1/users', (0, express_http_proxy_1.default)('https://mark.cristilex.com'));
-app.use('/api/v1/users', (0, express_http_proxy_1.default)('https://note.cristilex.com'));
-app.use('/api/v1/users', (0, express_http_proxy_1.default)('https://file.cristilex.com'));
-app.use('/api/v1/users', (0, express_http_proxy_1.default)('https://mount.cristilex.com'));
-app.use('/api/v1/users', (0, express_http_proxy_1.default)('https://expense.cristilex.com'));
+app.use('/api/v1/marks', (0, express_http_proxy_1.default)('https://mark.cristilex.com'));
+app.use('/api/v1/notes', (0, express_http_proxy_1.default)('https://note.cristilex.com'));
+app.use('/api/v1/files', (0, express_http_proxy_1.default)('https://file.cristilex.com'));
+app.use('/api/v1/mount', (0, express_http_proxy_1.default)('https://mount.cristilex.com'));
+app.use('/api/v1/expense', (0, express_http_proxy_1.default)('https://expense.cristilex.com'));
 app.listen(PORT, () => {
     signale.success(`Servidor corriendo en http://localhost:${PORT}`);
 });
