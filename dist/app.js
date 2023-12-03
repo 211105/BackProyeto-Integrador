df9586bafff248356493444ee95dc1597b9d44f1
nombre <211102@ids.upchiapas.edu.mx>
2023-12-02 23:33:03 -0600

    Ambientandooooooooo

--- dist/app.js
+++ dist/app.js
@@ -45,8 +45,8 @@ app.use((0, express_fileupload_1.default)());
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use('/api/v1/users', userRouter_1.userRoutes);
-app.use('/api/v1/weeklyAmount', weekly_amountRouter_1.weeklyAmountRoutes);
+app.use(userRouter_1.userRoutes);
+app.use(weekly_amountRouter_1.weeklyAmountRoutes);
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Corriendo en el puerto ${port}`);
--- src/app.ts
+++ src/app.ts
@@ -23,8 +23,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/weeklyAmount', weeklyAmountRoutes);
+app.use(userRoutes);
+app.use(weeklyAmountRoutes);
 
 
 
