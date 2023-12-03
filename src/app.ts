513e2100dc3c4dc69877e65405cf426470c59180
nombre <211102@ids.upchiapas.edu.mx>
2023-12-02 23:42:20 -0600

    ambientandoooooooo

--- src/app.ts
+++ src/app.ts
@@ -24,8 +24,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/expenses', expenseRouter);
+app.use(userRoutes);
+app.use(expenseRouter);
 
 
 
