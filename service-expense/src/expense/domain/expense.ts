a4823e2711005a23905bf4a56648e0ccbdc87e91
Miller Vidal <84791228+211102-Miller@users.noreply.github.com>
2023-12-07 00:26:29 -0600

    Delete service-expense/src directory

--- service-expense/src/expense/domain/expense.ts
+++
@@ -1,8 +0,0 @@
-export class Expense{
-    constructor(
-        public uuid:string,
-        public weekly_amount_uuid:string,
-        public category:string,
-        public amount:number
-    ){}
-}
